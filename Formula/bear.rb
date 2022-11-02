9004855e1168915f1d3837142df0d8079d4d283e
Duke <longwdl@gmail.com>
2022-11-02 14:04:49 +0800

    Update bear.rb to 3.0.19

--- Formula/bear.rb
+++ Formula/bear.rb
@@ -1,19 +1,19 @@
 class Bear < Formula
   desc "Generate compilation database for clang tooling"
   homepage "https://github.com/rizsotto/Bear"
-  url "https://github.com/rizsotto/Bear/archive/3.0.17.tar.gz"
-  sha256 "107f94e045d930e88f5f5b4b484c8df1bf4834722943525765c271e0b5b34b78"
+  url "https://github.com/rizsotto/Bear/archive/3.0.19.tar.gz"
+  sha256 "2fcfe2c6e029182cfc54ed26b3505c0ef12b0f43df03fb587f335afdc2ca9431"
   license "GPL-3.0-or-later"
   revision 4
   head "https://github.com/rizsotto/Bear.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "ccf78dbb909d60946f3f338fe6f176245f0be99e13134dbdf2d92f95cde0fad0"
-    sha256 arm64_big_sur:  "28107af41fe66bf9bcbde87b0023f5e0fdebe51801e16bf02ddd1d571b34e183"
-    sha256 monterey:       "14b06f7e5caf574e89e802df1ae45566c6f21707e0d992bbeef358ab510ed17c"
-    sha256 big_sur:        "9cb996e00e55ee16656371e4a39f94faae95474b014c6978ec479c8570657180"
-    sha256 catalina:       "eac61d41c1cc6281de935600b2919dd08741db825727e31edfe440fb8f126975"
-    sha256 x86_64_linux:   "fc611912c7068a7d53401ebdccdb6585a9dafb9f16ad3ec9cdf18855c1ed171f"
+    sha256 arm64_monterey: "46fda09b07595c10b4f1bae2cbd9f5c173e141cfb74ad0aa29c774ae209d4210"
+    sha256 arm64_big_sur:  "1840d46bfb570e651bbbfaca709a9872cf5a674125b3694213ac43b9bb756687"
+    sha256 monterey:       "52596f07897a556ef2e7ed8fe1410d472be84b1fb0824776bddf672854ea3a3e"
+    sha256 big_sur:        "5846258cbacc252bfd87a6d49b250e78e5408b513d7da02364e0c76596082115"
+    sha256 catalina:       "ff0993843631f66dc67851e65638331d7d038b2e8c12229485ac3e060000652f"
+    sha256 x86_64_linux:   "3c3fb34c3ea1936602996325f211f4c58d46d7660e0e3bf523a3498574150191"
   end
 
   depends_on "cmake" => :build
@@ -51,11 +51,9 @@ class Bear < Formula
       -DENABLE_FUNC_TESTS=OFF
     ]
 
-    mkdir "build" do
-      system "cmake", "..", *args, *std_cmake_args
-      system "make", "all"
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
